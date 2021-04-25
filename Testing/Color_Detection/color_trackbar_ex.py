8699cb8a4147335040c57b887643f68f5d8b58f3
João Alegria <joaoalegria31@ua.pt>
2021-04-25 16:56:56 +0100

    Updated version with mouse click selection

--- Testing/Color_Detection/color_trackbar_ex.py
+++ Testing/Color_Detection/color_trackbar_ex.py
@@ -6,9 +6,43 @@ import time
 def nothing(x):
     pass
 
-# import the image
-frame = cv.imread("lego_red.jpg")
+def resize(img,fx,fy):
+    height, width = img.shape[:2]
+    size = (int(width * fx), int(height * fy))  # bgr
+    img = cv.resize(img, size)
 
+    return img
+
+def getposHsv(event,x,y,flags,param):
+    global hsv_ret,color_h,color_s,color_v 
+    global l_h,u_h,l_v,u_v,u_s,l_s
+    
+    if event==cv.EVENT_LBUTTONDOWN:
+        hsv_ret = hsv[y,x]
+        color_h = hsv[y,x,0]
+        color_s = hsv[y,x,1]
+        color_v = hsv[y,x,2]
+        l_h = color_h - 10
+        u_h = color_h + 10
+        l_s = color_s - 10
+        u_s = color_s + 10
+        l_v = color_v - 10
+        u_v = color_v + 10
+
+
+        l_h = cv.setTrackbarPos("L - H", "Trackbars",l_h)
+        l_s = cv.setTrackbarPos("L - S", "Trackbars",l_s)
+        l_v = cv.setTrackbarPos("L - V", "Trackbars",l_v)
+        u_h = cv.setTrackbarPos("U - H", "Trackbars",u_h)
+        u_s = cv.setTrackbarPos("U - S", "Trackbars",u_s)
+        u_v = cv.setTrackbarPos("U - V", "Trackbars",u_v)
+
+
+# import the image
+frame = cv.imread("/home/alegria/VCI/OPENCV_COURSE/vciproj3/dataset2/rect/IMG_20210412_173340.jpg")
+
+# resize function
+frame = resize(frame,0.2,0.2)
 
 # Create a window named trackbars.
 cv.namedWindow("Trackbars")
@@ -23,12 +57,14 @@ cv.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
 cv.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
 cv.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
 cv.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
- 
+
+# Convert the BGR image to HSV image.
+cv.setMouseCallback("Trackbars",getposHsv)
+
 while True:
 
-    # Convert the BGR image to HSV image.
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    
+
     # Get the new values of the trackbar in real time as the user changes 
     # them
     l_h = cv.getTrackbarPos("L - H", "Trackbars")
@@ -46,35 +82,52 @@ while True:
     # Filter the image and get the binary mask, where white represents 
     # your target color
     mask = cv.inRange(hsv, lower_range, upper_range)
- 
+
     # You can also visualize the real part of the target color (Optional)
     res = cv.bitwise_and(frame, frame, mask=mask)
+
+
+    # load background (could be an image too)
+    bk = np.full(frame.shape, 255, dtype=np.uint8)  # white bk
+
+    # get masked background, mask must be inverted 
+    mask = cv.bitwise_not(mask)
+    bk_masked = cv.bitwise_and(bk, bk, mask=mask)
+
+    # combine masked foreground and masked background 
+    final = cv.bitwise_or(res, bk_masked)
     
+    mask = cv.bitwise_not(mask)  # revert mask to original
+
+
     # Converting the binary mask to 3 channel image, this is just so 
     # we can stack it with the others
     mask_3 = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
     
     # stack the mask, orginal frame and the filtered result
-    stacked = np.hstack((mask_3,frame,res))
+    stacked = np.hstack((mask_3,res,final))
     
     # Show this stacked frame at 40% of the size.
-    cv.imshow('Trackbars',cv.resize(stacked,None,fx=0.4,fy=0.4))
-    
+    cv.imshow("Trackbars",frame)
+    cv.imshow("stacked",stacked)
+
     # If the user presses ESC then exit the program
     key = cv.waitKey(1)
     if key == 27:
         break
+
     
-    # If the user presses `s` then print this array.
-    if key == ord('s'):
+    # # If the user presses `s` then print this array.
+    # if key == ord('s'):
         
-        thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
-        print(thearray)
+    #     thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
+    #     print(thearray)
         
-        # Also save this array as penval.npy
-        np.savetxt('hsv_value',thearray)
-        break
+    #     # Also save this array as penval.npy
+    #     np.savetxt('hsv_value',thearray)
+    #     break
+
+        
     
 # Release the camera & destroy the windows.    
-cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
