fa6320bd4d1ecec19f1adce8277088b665c1d29b
João Alegria <joaoalegria31@ua.pt>
2021-04-25 22:26:01 +0100

    name changed

---
+++ Testing/Color_Detection/color_detection_rect.py
@@ -0,0 +1,122 @@
+#finding hsv range of target object(pen)
+import cv2 as cv
+import numpy as np
+import time
+# in order to read the color json file
+import json
+
+#Letter font
+font = cv.FONT_HERSHEY_SIMPLEX
+
+
+"""
+Functions
+"""
+
+# A required callback method that goes into the trackbar function.
+def nothing(x):
+    pass
+
+
+def imageprocessing(frame,hsv_low,hsv_upper):
+
+    # Convert the BGR image to HSV image.
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # Set the lower and upper HSV range according to the value selected
+    # by the trackbar
+    lower_range = np.array(hsv_low)
+    upper_range = np.array(hsv_upper)
+
+    # Filter the image and get the binary mask, where white represents 
+    # your target color
+    mask = cv.inRange(hsv, lower_range, upper_range)
+
+    # You can also visualize the real part of the target color (Optional)
+    res = cv.bitwise_and(frame, frame, mask=mask)
+
+    # Converting the binary mask to 3 channel image, this is just so 
+    # we can stack it with the others
+    mask_3 = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+
+    # Morphological operation -> closing
+    kernel = np.ones((10,10),np.uint8)
+    closing = cv.morphologyEx(mask,cv.MORPH_CLOSE,kernel,iterations = 10)
+
+    return closing, res, mask_3
+
+
+def edge_finding(frame,closing,res):
+
+    # Find edges
+    masked_new = cv.bitwise_and(frame,frame,mask = closing)
+    # edge finding for the new image with closing
+    gray = cv.cvtColor(masked_new,cv.COLOR_BGR2GRAY)
+    # edge finding for the original image
+    edge = cv.Canny(gray,100,200)
+    gray_2 = cv.cvtColor(res,cv.COLOR_BGR2GRAY)
+    edge2 = cv.Canny(gray_2,100,200)
+
+    return edge, edge2
+
+def name_contour(closing,frame_contours):
+    # contours
+    contours, hierarchy = cv.findContours(closing, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)[-2:]
+    cnt = contours[0]
+    #cv.drawContours(frame_contours,contours,-1,(0,255),3)
+    x,y,w,h = cv.boundingRect(cnt)
+    frame_contours = cv.rectangle(frame_contours,(x,y),(x+w,y+h),(0,255,0),10)
+
+    # put text in the image identifying the color
+    cv.putText(frame_contours, hsv_color, (x, y),font,3,(0,255,0),5) 
+
+    return frame_contours
+
+"""
+    Main Program
+"""
+
+# import the image
+frame = cv.imread("/home/alegria/VCI/OPENCV_COURSE/vciproj3/dataset2/rect/IMG_20210412_173340.jpg")
+frame_contours = frame.copy()
+
+
+
+# read file
+with open('colors.json') as json_file:
+    data = json_file.read()
+
+# parse file
+obj = json.loads(data)
+
+for f in obj['colors']:
+
+    hsv_color = (f['color'])
+    hsv_low = (f['hsv_low'])
+    hsv_upper =(f['hsv_upper'])
+
+    closing,res, mask_3 = imageprocessing(frame,hsv_low,hsv_upper)
+    edge,edge2 = edge_finding(frame,closing,res)
+    frame_contours = name_contour(closing,frame_contours)
+
+    ## stack
+    # edge finding
+    stacked = np.hstack((edge,edge2))
+    # Morfological, Edges  operations
+    stacked_2 = np.hstack((closing,edge))
+    # initial photos
+    stacked_3 = np.hstack((mask_3,frame,res))
+
+    # Show this stacked frames
+    cv.imshow('Edge finding',cv.resize(stacked,None,fx=0.2,fy=0.2))
+    cv.imshow('Morfological, Edges operations',cv.resize(stacked_2,None,fx=0.2,fy=0.2))
+    cv.imshow('Initial Images',cv.resize(stacked_3,None,fx=0.2,fy=0.2))
+    cv.imshow('Contours finding and draw',cv.resize(frame_contours,None,fx=0.2,fy=0.2))
+
+
+
+# If the user presses ESC then exit the program
+key = cv.waitKey(0)
+if key == 27:
+    cv.destroyAllWindows()
+
