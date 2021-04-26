d8320503875052991d5ea1d66e93a7bca37ff73a
Raúl Kevin Viana <raulviana@ua.pt>
2021-04-26 12:23:16 +0100

    Dataset and new file

---
+++ Testing/Shape Detection Approaches/dataset_iterator/usefull_stuff.py
@@ -0,0 +1,60 @@
+import glob
+import cv2 as cv
+
+directory_str = 'dataset/'
+scale_percent = 20
+windowNameConfig = 'Input'
+directory_path = '../dataset_iterator/dataset/'
+
+def resize(img,s=scale_percent):
+    width = int(img.shape[1] * s / 100)
+    height = int(img.shape[0] * s / 100)
+    dim = (width, height)
+    return cv.resize(img, dim, interpolation=cv.INTER_AREA)
+
+def mod_pictures(img):
+    '''
+    Doesnt do nothing to the images
+
+    :param img:
+    :return:
+    '''
+    pass
+
+
+def window_with_trackbar(wName=windowNameConfig, dir_path=directory_path, mod_pics_funct=mod_pictures, scale_per=20):
+    '''
+    Shows original image and modifies the original image with a function and shows it
+
+    :param scale_per: Image scaling parameter
+    :param wName: Name of the input window
+    :param dir_path: directory with the path of dataset
+    :param mod_pics_funct: function that modifies the original image
+    :return:
+    '''
+
+    pics = glob.glob(dir_path + '*.jpg')
+    cv.namedWindow(wName)
+    cv.namedWindow('Output')
+    cv.createTrackbar('File',wName,0,len(pics)-1,change_file)
+
+    while(1):
+        picPos = cv.getTrackbarPos('File', wName)
+        filename = pics[picPos]
+
+        # Read Image
+        img = cv.imread(filename)
+
+        # Resize image
+        imgRe = resize(img,scale_per)
+
+        # Show original images
+        cv.imshow(windowNameConfig, imgRe)
+
+        newImg = mod_pics_funct(imgRe)
+
+        cv.imshow('Output', newImg)
+
+        if cv.waitKey(1) & 0xff == 27:
+            cv.destroyAllWindows()
+            break
