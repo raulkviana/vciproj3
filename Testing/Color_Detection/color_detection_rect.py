349d071497922eed3a08f56a3d913f15140aa1ab
João Alegria <joaoalegria31@ua.pt>
2021-05-09 12:18:24 +0100

    code commented

--- Testing/Color_Detection/color_detection_rect.py
+++ Testing/Color_Detection/color_detection_rect.py
@@ -1,24 +1,41 @@
-#finding hsv range of target object(pen)
+"""@package color detection rect
+Documentation for this module.
+
+This program as the objective of identifying only rect pieces based on their predefined HSV(Dictionary).
+"""
+
 import cv2 as cv
 import numpy as np
 import time
+
 # in order to read the color json file
 import json
 
 #Letter font
 font = cv.FONT_HERSHEY_SIMPLEX
 
-
 """
 Functions
 """
 
-# A required callback method that goes into the trackbar function.
+
 def nothing(x):
+    """
+    A required callback method that goes into the trackbar function.
+    """
     pass
 
-
 def imageprocessing(frame,hsv_low,hsv_upper):
+    """
+    Used to mask the desired region using HSV range
+    @param [in] frame : input image
+    @param [in] hsv_low : HSV lower range
+    @param [in] hsv_upper: HSV upper range
+
+    @param [out] mask_3 : masks the image in order to obtain the filter piece
+    @param [out] closing : morphological operation
+    @param [out] color_piece : visualize the real part of the target 
+    """
 
     # Convert the BGR image to HSV image.
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
@@ -33,7 +50,7 @@ def imageprocessing(frame,hsv_low,hsv_upper):
     mask = cv.inRange(hsv, lower_range, upper_range)
 
     # You can also visualize the real part of the target color (Optional)
-    res = cv.bitwise_and(frame, frame, mask=mask)
+    color_piece = cv.bitwise_and(frame, frame, mask=mask)
 
     # Converting the binary mask to 3 channel image, this is just so 
     # we can stack it with the others
@@ -43,23 +60,40 @@ def imageprocessing(frame,hsv_low,hsv_upper):
     kernel = np.ones((10,10),np.uint8)
     closing = cv.morphologyEx(mask,cv.MORPH_CLOSE,kernel,iterations = 10)
 
-    return closing, res, mask_3
+    return closing, color_piece, mask_3
 
 
-def edge_finding(frame,closing,res):
+def edge_finding(frame,closing,color_piece):
+    """
+    Edge finding function
+    @param [in] frame : input image
+    @param [in] closing : morphological operation
+    @param [in] color_piece : visualize the real part of the target 
+
+    @param [out] edge : edge finding for the original image 
+    @param [out] edge_color: edge finding in the color piece 
+    """
 
     # Find edges
     masked_new = cv.bitwise_and(frame,frame,mask = closing)
     # edge finding for the new image with closing
     gray = cv.cvtColor(masked_new,cv.COLOR_BGR2GRAY)
-    # edge finding for the original image
+    # edge finding for the original image 
     edge = cv.Canny(gray,100,200)
-    gray_2 = cv.cvtColor(res,cv.COLOR_BGR2GRAY)
-    edge2 = cv.Canny(gray_2,100,200)
+    #edge finding in the color piece 
+    gray_2 = cv.cvtColor(color_piece,cv.COLOR_BGR2GRAY)
+    edge_color = cv.Canny(gray_2,100,200)
 
-    return edge, edge2
+    return edge, edge_color
 
 def name_contour(closing,frame_contours):
+    """
+    Contour function
+    @param [in] closing : morphological operation
+    @param [in] frame_contours : image copy
+
+    @param [out] frame_countours : contours output
+    """
     # contours
     contours, hierarchy = cv.findContours(closing, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)[-2:]
     cnt = contours[0]
@@ -72,9 +106,6 @@ def name_contour(closing,frame_contours):
 
     return frame_contours
 
-"""
-    Main Program
-"""
 
 # import the image
 frame = cv.imread("/home/alegria/VCI/OPENCV_COURSE/vciproj3/dataset2/rect/IMG_20210412_173340.jpg")
@@ -83,7 +114,7 @@ frame_contours = frame.copy()
 
 
 # read file
-with open('colors.json') as json_file:
+with open('/home/alegria/VCI/OPENCV_COURSE/color_rect.json') as json_file:
     data = json_file.read()
 
 # parse file
@@ -95,17 +126,17 @@ for f in obj['colors']:
     hsv_low = (f['hsv_low'])
     hsv_upper =(f['hsv_upper'])
 
-    closing,res, mask_3 = imageprocessing(frame,hsv_low,hsv_upper)
-    edge,edge2 = edge_finding(frame,closing,res)
+    closing,color_piece, mask_3 = imageprocessing(frame,hsv_low,hsv_upper)
+    edge,edge_color = edge_finding(frame,closing,color_piece)
     frame_contours = name_contour(closing,frame_contours)
 
     ## stack
     # edge finding
-    stacked = np.hstack((edge,edge2))
+    stacked = np.hstack((edge,edge_color))
     # Morfological, Edges  operations
     stacked_2 = np.hstack((closing,edge))
     # initial photos
-    stacked_3 = np.hstack((mask_3,frame,res))
+    stacked_3 = np.hstack((mask_3,frame,color_piece))
 
     # Show this stacked frames
     cv.imshow('Edge finding',cv.resize(stacked,None,fx=0.2,fy=0.2))
@@ -119,4 +150,3 @@ for f in obj['colors']:
 key = cv.waitKey(0)
 if key == 27:
     cv.destroyAllWindows()
-
