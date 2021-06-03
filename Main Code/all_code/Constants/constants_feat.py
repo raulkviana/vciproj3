b5373bb62dce0e8e54260c8931b4acd2fb261de3
Raúl Kevin Viana <raulviana@ua.pt>
2021-06-03 20:30:44 +0100

    Update constants_feat

--- Main Code/all_code/Constants/constants_feat.py
+++ Main Code/all_code/Constants/constants_feat.py
@@ -8,4 +8,4 @@ This file has constant values for the FeatureExtrac module (feature_extrac.py)
 
 RESIZING_FACTOR = 0.15
 INPUT_FILE_UNIT_REF = "referenceParameter.npz"
-REAL_WORLD_LENGTH_LEGO = 2 # cm: Comprimento de um lego simples (1x1)
+REAL_WORLD_LENGTH_LEGO = 1.7/2 # cm: Comprimento de um lego simples (2x2)
